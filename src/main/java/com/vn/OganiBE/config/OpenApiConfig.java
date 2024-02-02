7ca55961477e0bc9b1e22076e3129769b62e1021
Lại Văn Tuyên <vantuyen24032001@gmail.com>
2024-02-02 17:04:33 +0700

    Ogani Backend

---
+++ src/main/java/com/vn/OganiBE/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.vn.OganiBE.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+    
+      return new OpenAPI()
+        .info(new Info().title("OGANI REST API DOCUMENT")
+        .contact(new Contact().name("Lại Văn Tuyên").email("vantuyen24032001@gmail.com").url("https://www.facebook.com/Tuyentuyn2403/"))
+        .termsOfService("http://swagger.io/terms/")
+        .license(new License().name("Apache 2.0")
+        .url("http://springdoc.org")));
+    }    
+}
